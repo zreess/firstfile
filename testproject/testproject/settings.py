eb5f0455c1a37d6c77d8bd157a89e4e7afba7487
zreess <sreerajkrihanan2222@gmail.com>
2026-03-19 11:11:13 +0530

    ok

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
