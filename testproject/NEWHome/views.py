e8d91c0240d3bd0137c79a9fdfe37fd92f2a130a
zreess <sreerajkrihanan2222@gmail.com>
2026-03-19 10:51:48 +0530

    ok

--- testproject/NEWHome/views.py
+++ testproject/NEWHome/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hey(request):
+    print("hello welcome back")
     fst="<h1>hello welcome back</h1>"
-    return HttpResponse(hey)
+    # return render (request,'one.html')
+    return HttpResponse(fst)
 
 
 def wow(request):
+    print("welcome to django")
     return render (request,'one.html')
